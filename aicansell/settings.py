bb72f72a3fa9b9d0b32b16ddd9c33ff6f650e3ee
Arindam <info@aicansell.com>
2023-09-01 14:55:53 +0530

    commit

--- aicansell/settings.py
+++ aicansell/settings.py
@@ -123,27 +123,13 @@ SIMPLE_JWT = {
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-      
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
+"""DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+        }}
 """
+
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
@@ -153,7 +139,7 @@ DATABASES = {
             'HOST': get_env_variable("DB_HOST"),
             'PORT': get_env_variable("DB_PORT"),
         }
-    }"""
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
