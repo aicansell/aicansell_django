e4b6e0549b0f8566ec2f8f80377efc54d9d8444b
Arindam <info@aicansell.com>
2023-09-01 09:25:35 +0530

    aws

--- aicansell/settings.py
+++ aicansell/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [' django-aicansell-env.eba-2a8k8khv.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-aicansell-env.eba-2a8k8khv.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
