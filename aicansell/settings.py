63d49dc56e4e57e83f0bd8a32d46a842319be147
Arindam <info@aicansell.com>
2023-09-03 10:52:12 +0530

    commit

--- aicansell/settings.py
+++ aicansell/settings.py
@@ -79,13 +79,13 @@ MIDDLEWARE = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_PORT = 587
-EMAIL_USE_TLS = True
+
 #EMAIL_USE_SSL = False
 
 
