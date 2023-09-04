54ad1d3f85914e029b6aa5e525b611b9d68c9b43
Arindam <info@aicansell.com>
2023-09-04 15:41:59 +0530

    role_scenario

--- organisation/admin.py
+++ organisation/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Org, Org_Roles
+from .models import Org, Org_Roles, Role_Scenario
 
 
 # Register your models here.
 admin.site.register(Org)
 
 admin.site.register(Org_Roles)
+
+admin.site.register(Role_Scenario)
