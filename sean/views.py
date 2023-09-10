4d2ca2354d0b9d2e9bef38ce0668eddb86afe6d9
Arindam <info@aicansell.com>
2023-09-10 07:13:36 +0530

    speech recognition

--- sean/views.py
+++ sean/views.py
@@ -19,6 +19,9 @@ from django.http import JsonResponse
 
 import speech_recognition as sr
 import pyttsx3
+import openai
+from decouple import config
+
 
 #from rest_framework import viewsets, generics
 
@@ -106,6 +109,44 @@ while(1):
          
     except sr.UnknownValueError:
         print("unknown error occurred")
+
+"""
+#whisper
+
+"""
+def recorder(self):
+    openai.api_key =config('api_key')
+   
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format= pyaudio.paInt16, channels = 1, rate = 44100, input= True, frames_per_buffer = 1024)
+    frames = []
+
+    try:
+        while True:
+            data = stream.read(1024)
+            frames.append(data)
+    except KeyboardInterrupt:
+        pass        
+
+    
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+
+    sound_file = wave.open("sean/myrecording.wav", "wb")
+    sound_file.setnchannels(1)
+    sound_file.setsamplewidth(audio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+    sound_file.writeframes(b"".join(frames))
+    sound_file.close()
+
+
+    audio_file = open("sean/myrecording.wav", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+
+    print(transcript)
 
 """
 
