b4c1cf6c8999b6ed89537d0ad1b11cbda429b01d
Arindam <info@aicansell.com>
2023-09-10 08:12:00 +0530

    speech recognition

--- sean/views.py
+++ sean/views.py
@@ -111,44 +111,7 @@ while(1):
         print("unknown error occurred")
 
 """
-#whisper
 
-"""
-def recorder(self):
-    openai.api_key =config('api_key')
-   
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format= pyaudio.paInt16, channels = 1, rate = 44100, input= True, frames_per_buffer = 1024)
-    frames = []
-
-    try:
-        while True:
-            data = stream.read(1024)
-            frames.append(data)
-    except KeyboardInterrupt:
-        pass        
-
-    
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-
-    sound_file = wave.open("sean/myrecording.wav", "wb")
-    sound_file.setnchannels(1)
-    sound_file.setsamplewidth(audio.get_sample_size(pyaudio.paInt16))
-    sound_file.setframerate(44100)
-    sound_file.writeframes(b"".join(frames))
-    sound_file.close()
-
-
-    audio_file = open("sean/myrecording.wav", "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-
-
-    print(transcript)
-
-"""
 
 @api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
