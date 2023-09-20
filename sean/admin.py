3bac99a42bfa462a03ff2e703005a114a2a5dcac
Arindam <info@aicansell.com>
2023-09-20 12:22:09 +0530

    commit

--- sean/admin.py
+++ sean/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Suggestion, PowerWords, NegativeWords, EmotionWords
+from .models import Item, Suggestion, PowerWords, NegativeWords, EmotionWords, Traits
 
 # Register your models here.
 
@@ -8,4 +8,5 @@ admin.site.register(Suggestion)
 admin.site.register(PowerWords)
 admin.site.register(NegativeWords)
 admin.site.register(EmotionWords)
+admin.site.register(Traits)
 
