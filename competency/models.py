7ad719ba2a6979451dcf9b53cef18970e1cbe279
Arindam <info@aicansell.com>
2023-09-26 15:28:35 +0530

    commit

--- competency/models.py
+++ competency/models.py
@@ -9,10 +9,7 @@ class Sub_Competency(models.Model):
     #sub_competency = models.ForeignKey(Competency, on_delete=models.CASCADE, related_name='role')
     subcompetency_name = models.CharField(max_length=250, unique = True)
 
-    def clean(self):
-        existing_entries = Sub_Competency.objects.exclude(id=self.id)
-        if existing_entries.filter(unique_field=self.unique_field).exists():
-            raise ValidationError({'unique_field': 'This field must be unique.'})
+    
 
 class Competency(models.Model):
     def __str__(self):
