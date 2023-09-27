31af3414a9460d2c3d30e99b4f973a8ea6c75741
Arindam <info@aicansell.com>
2023-09-27 16:12:51 +0530

    commit

--- aicansell/urls.py
+++ aicansell/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('chatbot/', include('chatbot.urls')),
     path('quiz/', include('quiz.urls')),
     path('sean/', include('sean.urls')),
-    path('org/', include('orgss.urls')),
+    #path('org/', include('organisation.urls')),
     path('', health_check),
     path('', include('competency.urls')),
     path('', include('words.urls')),
