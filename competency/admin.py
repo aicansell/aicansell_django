539a319636fc22a86614c208e957cfb71e779e2d
Arindam <info@aicansell.com>
2023-10-04 08:55:18 +0530

    new code without sean

--- competency/admin.py
+++ competency/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Competency, Sub_Competency
+from .models import Competency, Sub_Competency, Sub_Competency1
 
 
 # Register your models here.
 admin.site.register(Competency)
 admin.site.register(Sub_Competency)
+admin.site.register(Sub_Competency1)
