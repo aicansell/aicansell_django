c3e2b870c892523bc979ccaf9ac2bbdc6aa80106
KRITGYA2001 <kritgyakumar92@gmail.com>
2023-12-10 21:54:08 +0530

    changes

--- scenarios/models.py
+++ scenarios/models.py
@@ -5,6 +5,9 @@ from words.models import PowerWords, WeakWords
 class Scenarios(models.Model):
     item_name = models.CharField(max_length=300)
     item_emotions = models.TextField()
+    own_scenarios = models.TextField(null=True, blank=True)
+    thumbnail = models.FileField(upload_to='media/scenarios/thumbnail', null=True, blank=True)
+    coming_across_as = models.CharField(max_length=250, null=True, blank=True)
     power_words = models.ManyToOneRel(PowerWords, on_delete=models.CASCADE)
     weak_words = models.ManyToOneRel(WeakWords, on_delete=models.CASCADE)
     
