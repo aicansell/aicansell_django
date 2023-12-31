e59a928a8028d7502b9ece147c3ad407a859f1d9
KRITGYA2001 <kritgyakumar92@gmail.com>
2023-12-31 23:25:39 +0530

    refractoring the orgss model

--- orgss/models.py
+++ orgss/models.py
@@ -9,7 +9,7 @@ class Org(models.Model):
 
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=500, blank=True, null=True)
-    industry1 = models.ForeignKey(Industry, on_delete=models.CASCADE, related_name='industry1', default = 1)
+    industry = models.ForeignKey(Industry, on_delete=models.CASCADE, related_name='industry', default = 1)
 
 
 class Org_Roles(models.Model):
@@ -17,12 +17,12 @@ class Org_Roles(models.Model):
         return self.org_role_name
 
     org_role_name = models.CharField(max_length=250, blank=True, null=True)
-    org = models.ForeignKey(Org, on_delete=models.CASCADE, default = 1, related_name='org1')
+    org = models.ForeignKey(Org, on_delete=models.CASCADE, default = 1, related_name='org')
 
 
 class Weightage(models.Model):
     org_role = models.ForeignKey(Org_Roles, on_delete=models.CASCADE)
-    competency = models.ForeignKey(Competency, on_delete=models.CASCADE, default=1, related_name= 'rolecompetency1')
+    competency = models.ForeignKey(Competency, on_delete=models.CASCADE, default=1, related_name= 'rolecompetency')
     weightage = models.IntegerField(default=1)
     
     def __str__(self):
