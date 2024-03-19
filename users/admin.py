2e6b46b278c43eb358e1119fac67ac5607aac171
aicansell <info@aicansell.com>
2024-03-19 13:09:43 +0530

    changes

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import UserSubOrgs, UserMapping, UserRights, UserRightsMapping
-# Register your models here.
 
-admin.site.register(UserSubOrgs)
-admin.site.register(UserMapping)
-admin.site.register(UserRights)
-admin.site.register(UserRightsMapping)
+# Register your models here.
